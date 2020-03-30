ea5ce168b2a6bfb9d0dc9b3b83651222d4cdca65
Aaron <aaronsheoran@gmail.com>
2020-03-29 18:15:42 -0700

    got application working

--- js/search-bar.js
+++ js/search-bar.js
@@ -11,20 +11,29 @@ function searchForUser() {
         // it is an object with a key = results and a value = []
         // if the array is empty, no results
         // if the array is not empty, then we need to go through the array and pull out important info we care about to display on the results screen
-        var swapiData = response;
-        console.log(swapiData["results"]);
-        if(swapiData["count"]<0){
-            console.log('sdshd');
+        var swapiData = JSON.parse(response)        
+        if(swapiData["count"]>0){
        var name = swapiData.results[0].name
        var height = swapiData.results[0].height
        var mass = swapiData.results[0].mass
        var gender = swapiData.results[0].gender
-       var skincolor = swapiData.results[0].skin_color
+       var skin_color = swapiData.results[0].skin_color
        var homeworld = swapiData.results[0].homeworld
 
-       console.log(name, height, mass, gender, skincolor, homeworld);
-        }
-        // pull out into variables, the important attributes you care about from results
+
+       client.get(homeworld,(result) => {
+        var data = JSON.parse(result)
+
+
+        sessionStorage.setItem("name", name)
+        sessionStorage.setItem("height", height)
+        sessionStorage.setItem("mass", mass)
+        sessionStorage.setItem("gender", gender)
+        sessionStorage.setItem("skin_color", skin_color)
+        sessionStorage.setItem("homeworld", data.name)
+        window.location.href = "search-results.html"
+       })
+    }
 
     });
 }
--- js/search-results.js
+++ js/search-results.js
@@ -1,3 +1,14 @@
 
-let name = sessionStorage.getItem("name") 
+let name = sessionStorage.getItem("name")
+let mass = sessionStorage.getItem("mass") 
+let gender = sessionStorage.getItem("gender") 
+let skin_color = sessionStorage.getItem("skin_color") 
+let homeworld = sessionStorage.getItem("homeworld") 
+let height = sessionStorage.getItem("height") 
+
 document.getElementById("name").innerHTML = name
+document.getElementById("mass").innerHTML = `Mass: ${mass}`
+document.getElementById("gender").innerHTML = `Gender: ${gender}`
+document.getElementById("skin_color").innerHTML = `Skin Color: ${skin_color}`
+document.getElementById("homeworld").innerHTML = `Homeworld: ${homeworld}`
+document.getElementById("height").innerHTML = `Height: ${height}`
