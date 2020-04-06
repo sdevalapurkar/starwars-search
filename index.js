205321876e2e5346b3ba95e28c1001d765d50188
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-04-05 18:07:49 -0700

    fix issue with href

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 function getStarted() {
-    const location = window.location.pathname;
-    let newpath = location.slice(0, -10);
-    newpath += "html/login.html";
-    window.location.href = newpath;
+    window.location.href = "html/login.html";
 }
